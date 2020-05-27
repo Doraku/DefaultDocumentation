e26bbd1a91135e0b8ac9963af944a1a777ff8069
Laszlo Paillat <laszlo.paillat@gmail.com>
2020-05-27 22:52:08 +0200

    release 0.6.4

--- Release.bat
+++ Release.bat
@@ -1,6 +1,7 @@
 @ECHO off
 
 DEL /q package
-dotnet clean source\DefaultDocumentation.sln -c Release
+dotnet clean source\DefaultDocumentation\DefaultDocumentation.Release.csproj -c Release
 
+dotnet build source\DefaultDocumentation\DefaultDocumentation.Release.csproj -c Release
 dotnet pack source\DefaultDocumentation\DefaultDocumentation.Release.csproj -c Release -o package
