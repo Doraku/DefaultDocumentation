7d56d83237fbbc53dd2b82636ba7035e9aa64198
Laszlo Paillat <laszlo.paillat@gmail.com>
2021-09-04 18:44:37 +0200

    prefixed mutex name just in case

--- source/DefaultDocumentation.Common/Generator.cs
+++ source/DefaultDocumentation.Common/Generator.cs
@@ -7,7 +7,7 @@ namespace DefaultDocumentation
     {
         public static void Execute(Settings settings)
         {
-            using Mutex mutex = new(false, settings.OutputDirectory.FullName.Replace('\\', '|'));
+            using Mutex mutex = new(false, "DefaultDocumenation:" + settings.OutputDirectory.FullName.Replace('\\', '|'));
             if (!mutex.WaitOne(0))
             {
                 settings.Logger.Warn($"An other instance of DefaultDocumentation is trying to generate a documentation to the same output directory \"{settings.OutputDirectory.FullName}\", the current one will stop");
