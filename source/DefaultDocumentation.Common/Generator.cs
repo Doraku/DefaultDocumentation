405eb539603e738a33498cadaaff36c09a039a9b
Laszlo Paillat <laszlo.paillat@gmail.com>
2021-09-04 18:11:58 +0200

    fixed multiple DefaultDocumentation running with multiple TargetFrameworks

--- source/DefaultDocumentation.Common/Generator.cs
+++ source/DefaultDocumentation.Common/Generator.cs
@@ -1,9 +1,27 @@
-﻿using DefaultDocumentation.Writer;
+﻿using System.Threading;
+using DefaultDocumentation.Writer;
 
 namespace DefaultDocumentation
 {
     public static class Generator
     {
-        public static void Execute(Settings settings) => new MarkdownWriter(settings).Execute();
+        public static void Execute(Settings settings)
+        {
+            using Mutex mutex = new(false, settings.OutputDirectory.FullName.Replace('\\', '|'));
+            if (!mutex.WaitOne(0))
+            {
+                settings.Logger.Warn($"An other instance of DefaultDocumentation is trying to generate a documentation to the same output directory \"{settings.OutputDirectory.FullName}\", the current one will stop");
+                return;
+            }
+
+            try
+            {
+                new MarkdownWriter(settings).Execute();
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
     }
 }
