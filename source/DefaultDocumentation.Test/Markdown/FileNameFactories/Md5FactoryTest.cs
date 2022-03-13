4f647b8a43b725bcee227320b1366df08e44d5aa
Laszlo Paillat <laszlo.paillat@gmail.com>
2022-03-13 19:15:44 +0100

    added some more tests

---
+++ source/DefaultDocumentation.Test/Markdown/FileNameFactories/Md5FactoryTest.cs
@@ -0,0 +1,15 @@
+﻿using DefaultDocumentation.Markdown.FileNameFactories;
+using NFluent;
+using Xunit;
+
+namespace DefaultDocumentation.Markdown.Markdown.FileNameFactories
+{
+    public sealed class Md5FactoryTest : AFileNameFactoryTest<Md5Factory>
+    {
+        [Fact]
+        public void Name_Should_return_FullName() => Check.That(Name).IsEqualTo("Md5");
+
+        [Fact]
+        public void GetFileName_Should_return_Md5() => Test(AssemblyInfo.ClassDocItem, "07ucmh7NagP7qlcO1w9snQ==.md");
+    }
+}
