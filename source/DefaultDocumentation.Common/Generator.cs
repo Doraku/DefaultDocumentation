571e99b8adfff32a9bc421f6d7d8fc21ad5b060f
Raphael Siebenhofer <Raphael@hakom.at>
2022-05-30 12:49:30 +0200

    fixes #109

--- source/DefaultDocumentation.Common/Generator.cs
+++ source/DefaultDocumentation.Common/Generator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
@@ -115,13 +115,16 @@ namespace DefaultDocumentation
 
             _context = new GeneralContext(
                 _configuration,
-                new[] { typeof(Markdown.Writers.MarkdownWriter).Assembly.Location }
-                    .Concat(GetSetting<string[]>(nameof(settings.Plugins)) ?? Enumerable.Empty<string>())
-                    .Select(Assembly.LoadFrom)
+                new[] { typeof(Markdown.Writers.MarkdownWriter).Assembly }
+                    .Concat(
+                        (GetSetting<string[]>(nameof(settings.Plugins)) ?? Enumerable.Empty<string>())
+                        .Select(Assembly.LoadFrom)
+                    )
                     .SelectMany(a => a.GetTypes())
                     .ToArray(),
                 resolvedSettings,
                 DocItemReader.GetItems(resolvedSettings));
+
         }
 
         private void WritePage(DocItem item, StringBuilder builder)
