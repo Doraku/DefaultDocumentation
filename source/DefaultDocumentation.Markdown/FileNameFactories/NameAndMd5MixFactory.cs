a29025f09e289e8c8108c2b13167a8a679d52e41
Michael Adelson <mike.adelson314@gmail.com>
2022-08-11 08:03:18 -0400

    Remove unnecessary usings

--- source/DefaultDocumentation.Markdown/FileNameFactories/NameAndMd5MixFactory.cs
+++ source/DefaultDocumentation.Markdown/FileNameFactories/NameAndMd5MixFactory.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
+﻿using System.Linq;
 using DefaultDocumentation.Models;
 
 namespace DefaultDocumentation.Markdown.FileNameFactories
