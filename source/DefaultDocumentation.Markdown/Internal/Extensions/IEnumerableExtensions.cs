f3018b81dfab4c0677b906f0c004a7d23e0d9dd3
Laszlo Paillat <laszlo.paillat@gmail.com>
2025-09-27 15:18:02 +0200

    fixed ordering inconsistencies between netframework and net (core) execution

---
+++ source/DefaultDocumentation.Markdown/Internal/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq;
+
+internal static class IEnumerableExtensions
+{
+    public static IEnumerable<T> OrderBy<T>(this IEnumerable<T> source, Func<T, string> selector)
+        // Fixes inconsistencies between net framework and net (core) ordering.
+        => source.OrderBy(selector, StringComparer.OrdinalIgnoreCase);
+}
